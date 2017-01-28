93707cb3830de60e3e9c822e4a81010f0acbe2b2
BELHASSEN <bi106809@etu.unice.fr>
2017-01-28 14:31:55 +0100

    genre,schedule,liste de musique ...

--- MusicCity/app/src/main/java/com/al/music/musiccity/Menu2.java
+++ MusicCity/app/src/main/java/com/al/music/musiccity/Menu2.java
@@ -3,8 +3,10 @@ package com.al.music.musiccity;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -17,7 +19,12 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 12/01/2017.
@@ -27,7 +34,8 @@ public class Menu2 extends Fragment  {
     String a ;
     ListView lv;
     SearchView sv;
-    String[] teams = {"Pop", "Metal", "Rock", "Rap", "Rai", "Ragga", "Reggae", "Hip-Hop"};
+   // String[] teams = {"Pop", "Metal", "Rock", "Rap", "Rai", "Ragga", "Reggae", "Hip-Hop"};
+   public List<String> teams = new ArrayList<String>();
     ArrayAdapter<String> adapter;
 
     /*  partie music */
@@ -36,6 +44,60 @@ public class Menu2 extends Fragment  {
     Button buttonStop;
 
     String urlRecuperer;
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        new HttpRequestTask().execute();
+    }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Genre> {
+
+        @Override
+        protected Genre doInBackground(Void... params) {
+            try {
+                final String url = "http://al-zones.herokuapp.com/genres";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Genre greeting = restTemplate.getForObject(url, Genre.class);
+                //ResponseEntity<Greeting> responseEntity = restTemplate.postForEntity(url,new Greeting(),Greeting.class);
+
+                Log.e("MainActivity",greeting+"");
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Genre greeting) {
+            // TextView greetingContentText = (TextView) findViewById(R.id.content_value3);
+
+            Log.e("MainActivity est bien ",greeting.getGenres().toString());
+            teams=greeting.getGenres();
+            Toast.makeText(getContext(),"Liste de genre trouvée  " , Toast.LENGTH_LONG).show();
+            adapter.clear();
+            adapter.addAll(teams);
+            //  test=greeting.getContent();
+
+            // Toast.makeText(getContext(),greeting.getGenres().toString() + "récuperé ! " , Toast.LENGTH_LONG).show();
+
+            //  TextView greetingContentText = (TextView) getView().findViewById(R.id.content_value3);
+            //  greetingContentText.setText(test);
+
+           // teams=greeting.getGenres();
+          //  Toast.makeText(getContext(),teams.toString() + "récuperé dans teams ! " , Toast.LENGTH_LONG).show();
+
+
+
+        }
+
+
+    }
+
 
     @Nullable
     @Override
@@ -52,6 +114,7 @@ public class Menu2 extends Fragment  {
        final TextView GenreText = (TextView) view.findViewById(R.id.genre);
         adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, teams);
         lv.setAdapter(adapter);
+       // teams=greeting.getGenres();
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String text) {
@@ -73,7 +136,7 @@ public class Menu2 extends Fragment  {
             }
         });
        // urlRecuperer = "http://al-music-streamer.herokuapp.com/streamer/genre/"+a ;
-        urlRecuperer ="http://programmerguru.com/android-tutorial/wp-content/uploads/2013/04/hosannatelugu.mp3";
+       // urlRecuperer ="http://al-music-streamer.herokuapp.com/streamer/genre/"+a;
 
 
         buttonPlay = (Button) view.findViewById(R.id.play2);
@@ -91,7 +154,7 @@ public class Menu2 extends Fragment  {
                 mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
                 try {
-                    mPlayer.setDataSource(urlRecuperer);
+                    mPlayer.setDataSource("http://al-music-streamer.herokuapp.com/streamer/genre/"+a.toUpperCase());
                 } catch (IllegalArgumentException e) {
                     Toast.makeText(getContext(), "You might not set the URI correctly!", Toast.LENGTH_LONG).show();
                 } catch (SecurityException e) {
@@ -142,6 +205,7 @@ public class Menu2 extends Fragment  {
     public void onPause() {
         super.onPause();
     }
+
 
 
 
